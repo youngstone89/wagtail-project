3ccedd59fa88f6c247515c7deb702ed84d8171c0
KIM YEONG SEOK <youngstone89@gmail.com>
2018-01-19 08:36:16 +0900

    Added BlogCategory Class and Register Snippet for it. The next job would be corresponding modification on blog_page.html

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,14 +2,20 @@ from django.db import models
 
 # Create your models here.
 
-from modelcluster.contrib.taggit import ClusterTaggableManager
-from taggit.models import TaggedItemBase
+
+
 from wagtail.wagtailcore.models import Page, Orderable
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from wagtail.wagtailsearch import index
-from modelcluster.fields import ParentalKey
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from modelcluster.contrib.taggit import  ClusterTaggableManager
+from taggit.models import TaggedItemBase
+from wagtail.wagtailsnippets.models import register_snippet
+from django import forms
+from django.db import models
+
 
 
 class BlogPageTag(TaggedItemBase):
@@ -48,6 +54,7 @@ class BlogPage(Page):
 	intro = models.CharField(max_length=250)
 	body = RichTextField(blank=True)
 	tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
+	categories = ParentalManyToManyField('blog.BlogCategory', blank=True)
 
 	def main_image(self):
 		gallery_item = self.gallery_images.first()
@@ -65,11 +72,13 @@ class BlogPage(Page):
 
 		MultiFieldPanel([
 			FieldPanel('date'),
-			FieldPanel('tags')], heading='Blog Information'),
+			FieldPanel('tags'),
+			FieldPanel('categories', widget= 'forms.CheckboxSelectMultiple'),
+	],heading='Blog Information'),
 	
 	FieldPanel('intro'),
 	FieldPanel('body', classname="full"),
-	InlinePanel('gallery_images', label="Gallery Images")
+	InlinePanel('gallery_images', label="Gallery Images"),
 	]
 
 
@@ -80,3 +89,18 @@ class BlogPageGalleryImage(Orderable):
 	panels = [ ImageChooserPanel('image'),FieldPanel('caption')]
 
 
+
+@register_snippet
+class BlogCategory(models.Model):
+	name = models.CharField(max_length=255)
+	icon = models.ForeignKey('wagtailimages.Image',null=True, blank=True,on_delete=models.SET_NULL,related_name='+')
+
+	panels = [
+		FieldPanel('name'),
+		ImageChooserPanel('icon'),
+
+	]
+	def __str__(self):
+		return self.name
+	class Meta:
+		verbose_name_plural = 'blog categories'
